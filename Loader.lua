ccfbabaa0ee6c1efa48b8807b35fc32a316d824f
nothm_ <127741557+AfgMS@users.noreply.github.com>
2024-09-23 18:09:52 +0700

    Update Loader.lua
    Implemented Loader for Lime

--- Loader.lua
+++ Loader.lua
@@ -1,23 +1,15 @@
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local HttpService = game:GetService("HttpService")
-local MainFolder, ConfigFolder, LogsFolder = "Eternal", "Eternal/configs", "Eternal/logs"
-
-local Library = "https://raw.githubusercontent.com/AfgMS/EternalForRoblox/main/Library.lua"
-local BridgeDuels = "https://raw.githubusercontent.com/AfgMS/EternalForRoblox/main/BridgeDuels.lua"
-local Universal = "https://raw.githubusercontent.com/AfgMS/EternalForRoblox/main/Universal.lua"
+local MainFolder, ConfigFolder = "Lime", "Lime/configs"
 
 if not isfolder(MainFolder) then makefolder(MainFolder) end
 if not isfolder(ConfigFolder) then makefolder(ConfigFolder) end
-if not isfolder(LogsFolder) then makefolder(LogsFolder) end
 
-if isfolder(MainFolder) and isfolder(ConfigFolder) and isfolder(LogsFolder) then
-	if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then
-		writefile(LogsFolder .. "/support_issue_" .. game.PlaceId .. ".txt", "This game is not supported by Eternal")
-	elseif game.PlaceId == 11630038968 then
-		loadstring(game:HttpGet(BridgeDuels))()
+if isfolder(MainFolder) and isfolder(ConfigFolder) then
+	if game.PlaceId == 11630038968 then
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/LimeForRoblox/refs/heads/main/BridgeDuels.lua"))()
 	else
-		loadstring(game:HttpGet(Universal))()
-    writefile(LogsFolder .. "/comingsoon_" .. game.PlaceId .. ".txt", "Coming soon...")
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/LimeForRoblox/refs/heads/main/Universal.lua"))()
 	end
 end
