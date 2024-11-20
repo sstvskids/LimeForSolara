46a7991bd99c54ae7691dab06205890c7c902486
nothm_ <127741557+AfgMS@users.noreply.github.com>
2024-11-20 20:29:37 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -8,12 +8,7 @@ if not isfolder(ConfigFolder) then makefolder(ConfigFolder) end
 
 if isfolder(MainFolder) and isfolder(ConfigFolder) then
 	if game.PlaceId == 11630038968 then
-		if LocalPlayer.Backpack:FindFirstChildWhichIsA("Tool").Name:match("Apple") or LocalPlayer.Character:FindFirstChildWhichIsA("Tool").Name:match("Apple") then
-			loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/LimeForRoblox/refs/heads/main/BridgeDuel_Game.lua"))()
-			
-		else
-			loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/LimeForRoblox/refs/heads/main/BridgeDuel_Lobby.lua"))()
-		end
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/LimeForRoblox/refs/heads/main/BridgeDuel.lua"))()
 	else
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/LimeForRoblox/refs/heads/main/Universal.lua"))()
 	end
