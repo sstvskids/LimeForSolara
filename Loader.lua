dc3806204921a9adc777cd370570de2b59e075c2
nothm_ <127741557+AfgMS@users.noreply.github.com>
2025-01-01 23:07:11 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -7,7 +7,7 @@ if not isfolder(MainFolder) then makefolder(MainFolder) end
 if not isfolder(ConfigFolder) then makefolder(ConfigFolder) end
 
 if isfolder(MainFolder) and isfolder(ConfigFolder) then
-	if game.PlaceId == 11630038968 then
+	if game.PlaceId == 11630038968 or game.PlaceId == 10810646982 then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/LimeForRoblox/refs/heads/main/BridgeDuel.lua"))()
 	else
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/LimeForRoblox/refs/heads/main/Universal.lua"))()
