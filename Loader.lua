9bd2ef32386be7c3470bef2afd8f06667c4d2634
nothm_ <127741557+AfgMS@users.noreply.github.com>
2025-01-16 23:09:38 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,11 @@
 local Players = game:GetService("Players")
-local MainFolder, ConfigFolder = "Lime", "Lime/configs"
+local MainFolder, ConfigFolder, AssetsFolder = "Lime", "Lime/configs", "Lime/assets"
 
 if not isfolder(MainFolder) then makefolder(MainFolder) end
 if not isfolder(ConfigFolder) then makefolder(ConfigFolder) end
+if not isfolder(AssetsFolder) then makefolder(AssetsFolder) end
 
-if isfolder(MainFolder) and isfolder(ConfigFolder) then
+if isfolder(MainFolder) and isfolder(ConfigFolder) and isfolder(AssetsFolder) then
 	if game.PlaceId == 11630038968 or game.PlaceId == 10810646982 then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/LimeForRoblox/refs/heads/main/BridgeDuel.lua"))()
 	else
