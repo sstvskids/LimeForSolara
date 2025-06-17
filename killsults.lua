e72eb3977926797fe02bafb402ae4b7fe2dce725
nothm_ <127741557+AfgMS@users.noreply.github.com>
2025-06-17 20:57:34 +0700

    Create killsults.lua

---
+++ killsults.lua
@@ -0,0 +1,9 @@
+return {
+	"Better luck next time, {plr}",
+	"{plr} forgot how to play",
+	"{plr}, Ha Ha",
+	"{plr} = walking free kill",
+	"{plr} im just lagging",
+	"good game, {plr}",
+	"{plr} just got Lime'd 🍋‍🟩"
+}
