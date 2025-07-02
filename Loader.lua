be637e327078387c0ae2b7af805212376017e265
sstvskids <126665568+sstvskids@users.noreply.github.com>
2025-07-02 19:25:21 +1000

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -10,7 +10,7 @@ end
 -- require / debug checks
 if not (debug.getupvalue or debug.getconstants) then
 	shared.badexecs = true
-	return loadstring(game:HttpGet("https://raw.githubusercontent.com/sstvskids/LimeForRoblox/refs/heads/main/BridgeDuel.lua"))()
+	return loadstring(game:HttpGet("https://raw.githubusercontent.com/sstvskids/LimeForSolara/refs/heads/main/BridgeDuel.lua"))()
 end
 
 if require and (game.PlaceId == 11630038968 or game.PlaceId == 12011959048 or game.PlaceId == 14191889582 or game.PlaceId == 14662411059) then
@@ -18,17 +18,17 @@ if require and (game.PlaceId == 11630038968 or game.PlaceId == 12011959048 or ga
 	local suc, res = pcall(require, replicatedStorage.Blink.Client)
 	if suc == false then
 		shared.badexecs = true
-		return loadstring(game:HttpGet("https://raw.githubusercontent.com/sstvskids/LimeForRoblox/refs/heads/main/BridgeDuel.lua"))()
+		return loadstring(game:HttpGet("https://raw.githubusercontent.com/sstvskids/LimeForSolara/refs/heads/main/BridgeDuel.lua"))()
 	end
 elseif not require then
 	shared.badexecs = true
-	return loadstring(game:HttpGet("https://raw.githubusercontent.com/sstvskids/LimeForRoblox/refs/heads/main/BridgeDuel.lua"))()
+	return loadstring(game:HttpGet("https://raw.githubusercontent.com/sstvskids/LimeForSolara/refs/heads/main/BridgeDuel.lua"))()
 end
 
 if not shared.badexecs == true and (game.PlaceId == 11630038968 or game.PlaceId == 12011959048 or game.PlaceId == 14191889582 or game.PlaceId == 14662411059) then
 	return loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/LimeForRoblox/refs/heads/main/BridgeDuel.lua"))()
 else
-	return loadstring(game:HttpGet("https://raw.githubusercontent.com/sstvskids/LimeForRoblox/refs/heads/main/Universal.lua"))()
+	return loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/LimeForRoblox/refs/heads/main/Universal.lua"))()
 end
 
 -- stav :)
